637606b07bef1e5d218194f3012a06c2baf60f57
Quincy Reid <qreid51@gmail.com>
2013-06-10 15:36:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PRTracker::Application.config.secret_token = 'f0fda6f450b678a2bc512f620a0e0591e1e82121856205b46d9a332cce444b065e57366be8ae29e060939eeb4d46a6c43613ddafc6b8a177620daa5c09dd3eee'
