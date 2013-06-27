f10d92e6b6a977ac3e5241c10ffadb5ce1ec5c81
Quincy Reid <qreid51@gmail.com>
2013-06-27 01:55:24 -0400

    Adding page and adding bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PRTracker::Application.routes.draw do
+  get 'about' => 'pages#about'
+  
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
